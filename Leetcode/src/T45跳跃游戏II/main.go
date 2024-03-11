f7a084c08b2a19ab8917b85681a393462d2d719a
andrew <3476511424@qq.com>
2024-03-11 21:47:08 +0800

    跳跃游戏II

--- Leetcode/src/T45跳跃游戏II/main.go
+++ Leetcode/src/T45跳跃游戏II/main.go
@@ -5,6 +5,15 @@ func main() {
 }
 
 func jump(nums []int) int {
-
-	return 0
+	res := 0
+	for i := len(nums) - 1; i > 0; {
+		for j := 0; j < len(nums); j++ {
+			if nums[j] >= i-j {
+				i = j
+				res++
+				break
+			}
+		}
+	}
+	return res
 }
